87619d5162ddff584a77e6298ed1f698e7c4a188
FedmanCassad <vov_one@icloud.com>
2021-10-31 14:41:08 +0300

    fixes
    Fixed reviewer's remarks

--- Course2FinalTask/GeneralExtensions/UIView + CorneredShadow.swift
+++ Course2FinalTask/GeneralExtensions/UIView + CorneredShadow.swift
@@ -1,26 +1,26 @@
 import UIKit
 
 extension UIView {
-  
-  // TODO: А есть смысл в методе, который принимает столько значений? Он ничего не облегчает, лучше бы он по умолчанию какие-то параметры инициализировал, чтобы можно было их как-то при желании менять во время инициализации, но чтобы это не было обязательным.
-	func addCornerEffects(
-		cornerRadius: CGFloat = 0,
-		fillColor: UIColor = .white,
-		shadowColor: UIColor = .clear,
-		shadowOffset: CGSize,
-		shadowOpacity: Float,
-		shadowRadius: CGFloat,
-		borderColor: UIColor,
-		borderWidth: CGFloat
-	) {
-		self.layer.cornerRadius = cornerRadius
-		self.layer.shadowColor = shadowColor.cgColor
-		self.layer.shadowOffset = shadowOffset
-		self.layer.shadowRadius = shadowRadius
-		self.layer.shadowOpacity = shadowOpacity
-		self.layer.borderColor = borderColor.cgColor
-		self.layer.borderWidth = borderWidth
-		self.layer.backgroundColor = nil
-		self.layer.backgroundColor = fillColor.cgColor
-	}
+
+    // RESOLVED
+    func addCornerEffects(
+        cornerRadius: CGFloat = 5,
+        fillColor: UIColor = .white,
+        shadowColor: UIColor = .black.withAlphaComponent(0.2),
+        shadowOffset: CGSize = .zero,
+        shadowOpacity: Float = 1.0,
+        shadowRadius: CGFloat = 25.0,
+        borderColor: UIColor = .clear,
+        borderWidth: CGFloat = 0
+    ) {
+        self.layer.cornerRadius = cornerRadius
+        self.layer.shadowColor = shadowColor.cgColor
+        self.layer.shadowOffset = shadowOffset
+        self.layer.shadowRadius = shadowRadius
+        self.layer.shadowOpacity = shadowOpacity
+        self.layer.borderColor = borderColor.cgColor
+        self.layer.borderWidth = borderWidth
+        self.layer.backgroundColor = nil
+        self.layer.backgroundColor = fillColor.cgColor
+    }
 }
--- Course2FinalTask/Helpers/DynamicBinder.swift
+++ Course2FinalTask/Helpers/DynamicBinder.swift
@@ -1,25 +1,32 @@
 import Foundation
 
-// TODO: Лучше написать документацию о том, что это за класс, для чего служит и когда применяется. Его открытые свойства и методы тоже стоит описать.
 class Dynamic<T> {
-  typealias Listener = (T) -> Void
-  var listener: Listener?
-  var value: T {
-    didSet {
-      listener?(value)
+    /// Dynamic class is kind of wrapper class to make viewModel-model bindings easier by reducing
+    /// count of closure declarations in viewModel. The value encapsulated in Dynamic<T> class becomes something like 'observable',
+    /// we bind some code to execute in case of changing encapsulated value.
+    // RESOLVED: - Позанимался немного английским.
+    typealias Listener = (T) -> Void
+
+    /// - listener: the closure which is going to be executed in case of receiving value.
+    var listener: Listener?
+
+    /// - value: observable value
+    var value: T {
+        didSet {
+            listener?(value)
+        }
     }
-  }
 
-  init(_ val: T) {
-    value = val
-  }
+    init(_ val: T) {
+        value = val
+    }
 
-  func bind(listener: Listener?) {
-    self.listener = listener
-  }
+    func bind(listener: Listener?) {
+        self.listener = listener
+    }
 
-  func bindAndPerform(listener: Listener?) {
-    self.listener = listener
-    listener?(value)
-  }
+    func bindAndPerform(listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
 }
